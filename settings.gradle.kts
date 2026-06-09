9e3dbea8e57e7dd31bc6d7719bb6dec6b50ffeae
XenorKing <da1ysss362@gmail.com>
2026-06-09 17:15:03 +0300

    chore: rename project to NovMusic

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,18 @@
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+      repositories {
+          google()
+          mavenCentral()
+          gradlePluginPortal()
+      }
+  }
+  dependencyResolutionManagement {
+      repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+      repositories {
+          google()
+          mavenCentral()
+      }
+  }
 
-rootProject.name = "NovMusica"
-include(":app")
+  rootProject.name = "NovMusic"
+  include(":app")
+  
